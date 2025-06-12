4eedd48f24a4f3e5361448af12b91b7374d6bebd
BAEK710 <sly3784@gmail.com>
2025-06-12 14:36:24 +0900

    찐막

--- problems.py
+++ problems.py
@@ -4,10 +4,10 @@ problems = [
     {"word": "인공지능", "difficulty": "쉬움"},
     {"word": "컴퓨터", "difficulty": "쉬움"},
     {"word": "고양이", "difficulty": "쉬움"},
-    {"word": "세금", "difficulty": "중간"},
-    {"word": "의사소통", "difficulty": "중간"},
-    {"word": "우정", "difficulty": "중간"},
-    {"word": "정의", "difficulty": "어려움"},
-    {"word": "탈진", "difficulty": "어려움"},
-    {"word": "교제", "difficulty": "어려움"}
+    {"word": "고등학교", "difficulty": "중간"},
+    {"word": "행복", "difficulty": "중간"},
+    {"word": "마법", "difficulty": "중간"},
+    {"word": "연필", "difficulty": "어려움"},
+    {"word": "노트북", "difficulty": "어려움"},
+    {"word": "여자친구", "difficulty": "어려움"}
 ]
